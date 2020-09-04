a9722aa99922f6d6df1f2bcbe580a55f4d0b870c
Bryan Yick <bryan_yc@yahoo.com>
2020-09-04 19:34:18 +0800

    added some comments for help

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose")
 // var mongoDB = "mongodb+srv://ycby:jiMiYB6xRikGpiJ@cluster0.87w15.gcp.mongodb.net/fontes?retryWrites=true&w=majority";
+//the local one has the latest changes. this is the location information sometimes being read as a date fix.
 var mongoDB = "mongodb://127.0.0.1:27017/fontes";
 
 mongoose.connect(mongoDB, {useUnifiedTopology: true, useNewUrlParser: true});
