d43017f3a3bded849edca207e7f34e214be498ec
Patrick McCann <pgm5@st-andrews.ac.uk>
2020-12-07 12:01:54 +0000

    Towards containerisation. Runs, but no search results

--- db.js
+++ db.js
@@ -1,9 +1,27 @@
 var mongoose = require("mongoose")
 
-//the local one has the latest changes. this is the location information sometimes being read as a date fix.
-var mongoDB = "mongodb://127.0.0.1:27017/fontes";
+const {
+  MONGO_HOSTNAME,
+  MONGO_PORT,
+  MONGO_DB
+} = process.env;
 
-mongoose.connect(mongoDB, {useUnifiedTopology: true, useNewUrlParser: true});
+const options = {
+  useNewUrlParser: true,
+  connectTimeoutMS: 10000,
+  useUnifiedTopology: true
+};
+
+//the local one has the latest changes. this is the location information sometimes being read as a date fix.
+var mongoDB = `mongodb://${MONGO_HOSTNAME}:${MONGO_HOSTNAME}/${MONGO_DB}`;
+
+//mongoose.connect(mongoDB, {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect(mongoDB, options).then( function() {
+  console.log('MongoDB is connected');
+})
+  .catch( function(err) {
+  console.log(err);
+});
 
 var db = mongoose.connection;
 
