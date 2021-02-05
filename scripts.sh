19c632afaeaf654fc53e13277eb79217da2752ba
Patrick McCann <pgm5@st-andrews.ac.uk>
2021-02-05 16:25:34 +0000

    Fixes broken pagination for bibliographies

--- scripts.sh
+++ scripts.sh
@@ -3,4 +3,5 @@
 uglifyjs public/javascripts/all.js -c -m -d baseUrl="'$1'" -o public/javascripts/minified/all-min.js
 uglifyjs public/javascripts/searchpage.js -c -m -d baseUrl="'$1'" -o public/javascripts/minified/searchpage-min.js
 uglifyjs public/javascripts/bibliographypage.js -c -m -d baseUrl="'$1'" -o public/javascripts/minified/bibliographypage-min.js
+uglifyjs public/javascripts/pagination-links.js -c -m -d baseUrl="'$1'" -o public/javascripts/minified/pagination-links-min.js
 
