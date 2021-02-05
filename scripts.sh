0337b3c15af01d0eea8328efe9334e4f04492469
Patrick McCann <pgm5@st-andrews.ac.uk>
2021-02-05 15:12:00 +0000

    Allows a baseUrl of '/'

--- scripts.sh
+++ scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-uglifyjs public/javascripts/all.js -c -m -d baseUrl="$1" -o public/javascripts/minified/all-min.js
-uglifyjs public/javascripts/searchpage.js -c -m -d baseUrl="$1" -o public/javascripts/minified/searchpage-min.js
-uglifyjs public/javascripts/bibliographypage.js -c -m -d baseUrl="$1" -o public/javascripts/minified/bibliographypage-min.js
+uglifyjs public/javascripts/all.js -c -m -d baseUrl="'$1'" -o public/javascripts/minified/all-min.js
+uglifyjs public/javascripts/searchpage.js -c -m -d baseUrl="'$1'" -o public/javascripts/minified/searchpage-min.js
+uglifyjs public/javascripts/bibliographypage.js -c -m -d baseUrl="'$1'" -o public/javascripts/minified/bibliographypage-min.js
 
