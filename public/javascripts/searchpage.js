b5f67d5ea6abb1c2a2cb97aa3c5cfaca590166ca
Bryan Y <bryan_yc@yahoo.com>
2021-02-11 15:59:07 +0800

    updated some functionality and css

--- public/javascripts/searchpage.js
+++ public/javascripts/searchpage.js
@@ -6,6 +6,9 @@ $(document).ready(function() {
     //copy citation
     $(".cite").find(".copy").click(clipboardCopy);
 
+    //copy popup
+    $(".cite").find(".popup").click(copyClicked);
+
     //write see all records href
     $("#all-records").attr("href", replaceAllRecordsHref());
 
@@ -243,6 +246,13 @@ function fallbackCopy(thisButton) {
     document.execCommand("copy");
 
     hiddenInput.remove();
+}
+
+//Add popup when copy is clicked
+function copyClicked() {
+
+    console.log("clicked in popup");
+    $(this).find(".popuptext").toggleClass("show-popup");
 }
 
 function getCiteButtonNumber(citeButton) {
